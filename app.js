c8f5cdf767f08aacd5a404145cb7eece81f254f2
Tiffany Tsou <tiffanytsou@TiffanydeMacBook.local>
2020-12-15 12:25:32 +0800

    remove test_route

--- app.js
+++ app.js
@@ -13,8 +13,7 @@ app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 app.use('/',
     [
         require('./server/routes/admin_route'),
-        require('./server/routes/franchise_route'),
-        require('./server/routes/test_route')
+        require('./server/routes/franchise_route')
     ]
 )
 
