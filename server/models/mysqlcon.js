96e43d02a9bf8c1a1c937a42dd7e371663855061
Tiffany Tsou <tiffanytsou@TiffanydeMacBook.local>
2021-01-24 00:18:45 +0800

    update mysql connection config

--- server/models/mysqlcon.js
+++ server/models/mysqlcon.js
@@ -1,37 +1,32 @@
 const mysql = require("mysql");
-const env = process.env.NODE_ENV || "local";
+const env = process.env.NODE_ENV;
 const { promisify } = require("util");
 const { LOCAL_DB_HOST, LOCAL_DB_USER, LOCAL_DB_PWD, LOCAL_DB } = process.env;
-// const { AWS_DB_HOST, AWS_DB_USER, AWS_DB_PWD, AWS_DB } = process.env;
+const { AWS_DB_HOST, AWS_DB_USER, AWS_DB_PWD, AWS_DB } = process.env;
 // const { TEST_DB_HOST, TEST_DB_USER, TEST_DB_PWD, TEST_DB } = process.env;
 
-// const mysqlConfig = {
-//     production: { // for EC2 machine
-//         host: AWS_DB_HOST,
-//         user: AWS_DB_USER,
-//         password: AWS_DB_PWD,
-//         database: AWS_DB
-//     },
-//     development: { // for localhost development
-//         host: LOCAL_DB_HOST,
-// user: LOCAL_DB_USER,
-// password: LOCAL_DB_PWD,
-// database: LOCAL_DB
-//     },
+const mysqlConfig = {
+    production: { // for EC2 machine
+        host: AWS_DB_HOST,
+        user: AWS_DB_USER,
+        password: AWS_DB_PWD,
+        database: AWS_DB
+    },
+    development: { // for localhost development
+        host: LOCAL_DB_HOST,
+        user: LOCAL_DB_USER,
+        password: LOCAL_DB_PWD,
+        database: LOCAL_DB
+    }
 //     test: { // for automation testing (command: npm run test)
 //         host: DB_HOST,
 //         user: DB_USER,
 //         password: DB_PWD,
 //         database: TEST_DB
 //     }
-// };
+};
 
-const mysqlCon = mysql.createConnection({
-    host: LOCAL_DB_HOST,
-    user: LOCAL_DB_USER,
-    password: LOCAL_DB_PWD,
-    database: LOCAL_DB,
-});
+const mysqlCon = mysql.createConnection(mysqlConfig[env]);
 
 const promiseQuery = promisify(mysqlCon.query).bind(mysqlCon);
 const promiseTransaction = promisify(mysqlCon.beginTransaction).bind(mysqlCon);
